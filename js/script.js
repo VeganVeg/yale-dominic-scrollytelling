b801f28084facf0ef02e9d928d0507c6403500a1
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2024-05-09 19:04:49 -0400

    dw

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ const anim = gsap.fromTo('.wheel', {
 });
 
 
-/* ANIMATION CHAPITRE-1 */
+/* ANIMATIONS CHAPITRE-1 & 2 */
 gsap.timeline({
     scrollTrigger: {
         pin: true,
@@ -70,8 +70,11 @@ gsap.timeline({
     .fromTo('#chapitre-2', { opacity: '100%', }, { opacity: '0%', duration: 3 }, '-=0.5')
 
 
+
+/* ANIMATIONS CHAPITRE-3 */
 gsap.timeline({
     scrollTrigger: {
+
         pin: true,
         start: "top top",
         end: "bottom top",
@@ -79,8 +82,7 @@ gsap.timeline({
         trigger: "#chapitre-3",
     }
 })
-
-    .from('.chp3-t1', { y: '1vh', opacity: 0, duration: 1.5 }, '-=1')
+    .from('.chp3-t1', { y: '1vh', opacity: '0%', duration: 1.5 }, '-=1')
     .fromTo('.casque-pince', { y: '-25vh' }, { y: '-7vh', duration: 1.5, ease: 'back.out', }, '-=1')
     .fromTo('.casque-pince', { y: '-7vh' }, { y: '7.4vh', duration: 1.5, ease: 'back.in', delay: 1 })/* 43.4vh */
     .fromTo('.chp3-t1', { y: '0' }, { y: '1vh', opacity: 0, duration: 1.5 }, '+=4')
@@ -88,6 +90,8 @@ gsap.timeline({
     .fromTo('#chapitre-3', { opacity: '100%', }, { opacity: '0%', duration: 0.3 }, '-=0.5')
 
 
+
+/* ANIMATIONS CHAPITRE-4 & 5 */
 gsap.timeline({
     scrollTrigger: {
         scrub: 1,
@@ -98,16 +102,16 @@ gsap.timeline({
         trigger: "#chapitre-4",
     }
 })
-    .from('.chp4-t1', { y: '1vh', opacity: 0, duration: 4.5, })
+    .from('.chp4-t1', { y: '1vh', opacity: 0, duration: 8, delay: 2 })
     .fromTo('#chapitre-4 .kevin', { x: '-28vw' }, { x: '0', ease: 'sine.out', duration: 25 }, '+=5')
-    .fromTo('.chp4-t1', { y: '0' }, { y: '1vh', opacity: 0, duration: 4.5 }, '+=4')
+    .fromTo('.chp4-t1', { y: '0' }, { y: '1vh', opacity: 0, duration: 8 }, '+=4')
     .from('.chp4-t2', { y: '1vh', opacity: 0, duration: 8 })
     .from('#chapitre-4 .escalier', { rotate: 135, transformOrigin: "100% 10%", ease: 'sine.out', duration: 30 })
     .fromTo('#chapitre-4 .kevin', { x: '0' }, { x: '28vw', ease: 'sine.inOut', duration: 30 })
     .fromTo('#chapitre-4 .kevin', { x: '28vw' }, { x: '50vw', y: '-35vh', ease: 'sine.inOut', duration: 30 })
     .from('#chapitre-4 .escalier', { rotate: 0, transformOrigin: "100% 10%", ease: 'sine.out', duration: 30 })
     .from('#chapitre-4 .spritesheet-feu', { opacity: 0, duration: 20 })
-    .fromTo('.chp4-t2', { y: '0' }, { y: '1vh', opacity: 0, duration: 4.5 })
+    .fromTo('.chp4-t2', { y: '0' }, { y: '1vh', opacity: 0, duration: 8 })
     .from('.nuage2', { opacity: 0, scale: 0.5, duration: 20 }, '-=1.5')
     .from('.nuage1', { opacity: 0, scale: 0.5, duration: 20 }, '-=1.5')
     .fromTo('#chapitre-4 .fusee-ensemble', { y: '100vh' }, { y: '0', duration: 30, ease: 'sine.in' })
@@ -124,24 +128,22 @@ gsap.timeline({
     .from('.kevin5', { scale: 0, duration: 0.3 })
     .from('.chap-5 .meteorite', { scale: 0, duration: 0.3 })
     .from('.chap-5 .spritesheet-volant', { scale: 0, duration: 0.3 })
-    .fromTo('#chapitre-4 .nuage4', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' })
-    .fromTo('#chapitre-4 .nuage6', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' }, '-=15')
-    .fromTo('#chapitre-4 .nuage3', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' }, '-=15')
+    .fromTo('#chapitre-4 .nuage6', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' })
     .fromTo('#chapitre-4 .nuage7', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' }, '-=15')
     .fromTo('#chapitre-4 .nuage5', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' }, '-=15')
+    .fromTo('#chapitre-4 .nuage4', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' }, '-=15')
+    .fromTo('#chapitre-4 .nuage3', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' }, '-=15')
     .fromTo('#chapitre-4 .nuage8', { scale: 1 }, { scale: 0.5, opacity: 0, duration: 40, ease: 'back.in' }, '-=15')
-    .from('.chp5-t1', { y: '1vh', opacity: 0, duration: 8, delay: 15 })
+    .from('.chp5-t1', { y: '1vh', opacity: 0, duration: 9, delay: 15 })
     .fromTo('.chp5-t1', { y: '0' }, { y: '1vh', opacity: 0, duration: 9 }, '+=14')
-    .from('.chp5-t2', { y: '1vh', opacity: 0, duration: 8, delay: 5 })
+    .from('.chp5-t2', { y: '1vh', opacity: 0, duration: 9, delay: 5 })
     .fromTo('.chp5-t2', { y: '0' }, { y: '1vh', opacity: 0, duration: 9 }, '+=14')
-    .from('.chp5-t3', { y: '1vh', opacity: 0, duration: 8, })
+    .from('.chp5-t3', { y: '1vh', opacity: 0, duration: 9, })
     .fromTo('.chap-5 .bombe', { y: '40vh' }, { y: '5vh', duration: 30, ease: 'back.out' }, '+=10')
     .fromTo('.chp5-t3', { y: '0' }, { y: '1vh', opacity: 0, duration: 9 }, '+=25')
     .fromTo('#chapitre-5', { opacity: '100%', }, { opacity: '0%', duration: 0.3, }, '-=0.5')
-    .fromTo('.chap-5 .meteorite', { x: '-1vw' }, { x: '15vw', y: '5vh', rotate: -90, duration: 270, delay: 420 }, 0)
+    .fromTo('.chap-5 .meteorite', { x: '-1vw' }, { x: '15vw', y: '5vh', rotate: -90, duration: 270, delay: 440 }, 0)
     .fromTo('#chapitre-4', { opacity: '100%', }, { opacity: '0%', duration: 0.8 }, '-=0.5')
-
-
 
 gsap.fromTo('#path1', {
     drawSVG: "0% 0%",
@@ -166,6 +168,7 @@ gsap.fromTo('#path2', {
 
 
 
+/* ANIMATIONS CHAPITRE-6 */
 gsap.timeline({
     scrollTrigger: {
         scrub: 1,
@@ -194,12 +197,14 @@ gsap.timeline({
         }, duration: 30,
         ease: 'sine.in'
     })
-    .to('#bombe', { duration: 6, morphSVG: { shape: "#explosion", shapeIndex: 1, ease: 'sine.out' } }, '=+15')
+    .to('#bombe', { duration: 6, morphSVG: { shape: "#explosion", shapeIndex: 6, ease: 'sine.out' } }, '=+15')
     .fromTo('#svg', { scale: 1 }, { scale: 20, duration: 25 }, '-=6')
     .fromTo('.explosion2', { opacity: 0, scale: 1 }, { opacity: 100, transformOrigin: '50.5% 66%', scale: 21, duration: 25 }, '-=25')
     .from('#chapitre-6 .stun', { opacity: 0, duration: 25 }, '-=20')
 
 
+
+/* ANIMATIONS CHAPITRE-7 */
 gsap.timeline({
     scrollTrigger: {
         scrub: 1,
